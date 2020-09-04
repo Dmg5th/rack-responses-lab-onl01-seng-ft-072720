ed7a5f3efea279528291428e8ec9242d74b02188
Daniel Gould <dmgould310@gmail.com>
2020-09-04 19:31:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,10 @@ class Application
   
   time = Time.now.hour
   
-  if time >= 12
-    resp.write "Good Afternoon!"
-  else 
+  if time <= 12
     resp.write "Good Morning!"
+  else 
+    resp.write "Good Afternoon!"
   end 
     resp.finish 
   
